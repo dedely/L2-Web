523ace98940f13592463fc5a92683543b91eef9e
knbernard <60677447+knbernard@users.noreply.github.com>
2020-04-03 08:57:33 +0200

    Add files via upload

---
+++ src/include/header.inc.php
@@ -0,0 +1,32 @@
+<?php
+    include "include/functions.inc.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title><?php echo $title ?></title>
+        <meta name="author" content="Adel Abbas, Kevin Bernard" />
+        <meta name="date" content=<?php echo "\"" . date('D, d M Y') . "\"" ?> />
+        <meta name="keywords" content="Weather forecast, Region, City, CY, <?php echo $title ?>" />
+        <meta name="description" content="" />
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="standard.css" />
+    </head>
+
+    <body>
+        <header>
+           <div class="container">
+                <img src="./images/clouds2.jpg" alt="Clouds"/>
+                <div class="centered">
+                    <h1>Weather forecast</h1>
+                </div>
+            </div>
+            <nav>
+                <ul id="nav">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="comingsoon.php">Stats</a></li>
+                </ul>
+            </nav>
+        </header>
+        
